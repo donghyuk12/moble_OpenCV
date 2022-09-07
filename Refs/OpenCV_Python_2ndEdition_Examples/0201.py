12eb5b4048a85f5cd47dbf6a02fb6cfd39ca4a5e
donghyuk12 <tto108@naver.com>
2022-09-07 15:41:10 +0900

    0907

--- Refs/OpenCV_Python_2ndEdition_Examples/0201.py
+++ Refs/OpenCV_Python_2ndEdition_Examples/0201.py
@@ -1,13 +1,14 @@
 # 0201.py
 import cv2
 import numpy as np
-
+# from PIL import Image
+# import numpy as np
 imageFile = './data/lena.jpg'
 img  = cv2.imread(imageFile)    # cv2.IMREAD_COLOR
 img2 = cv2.imread(imageFile, 0) # cv2.IMREAD_GRAYSCALE
 
-##encode_img = np.fromfile(imageFile, np.uint8)
-##img = cv2.imdecode(encode_img, cv2.IMREAD_UNCHANGED)
+#encode_img = np.fromfile(imageFile, np.uint8)
+#img = cv2.imdecode(encode_img, cv2.IMREAD_UNCHANGED)
 
 cv2.imshow('Lena color',img)
 cv2.imshow('Lena grayscale',img2)
@@ -15,3 +16,16 @@ cv2.imshow('Lena grayscale',img2)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
+# im = Image.open('./data/lena.jpg')
+# size = (256, 256)
+# im.thumbnail(size)
+
+# mask_ = np.array(im)
+# for i in range(len(mask_)):
+#     for j in range(len(mask_[i])):
+#         if np.sum(mask_[i][j])<45:
+#             for k in range(3):
+#                 mask_[i][j][k] = 255
+# Image.fromarray(mask_, 'RGB')
+# cv2.imwrite('./data/llena.jpg', mask_)
+# print(mask_.shape)
